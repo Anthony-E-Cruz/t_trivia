8fc6dadfbd90bbaf1e23c5c25b229cc192e3e7fb
Anthony-E-Cruz <anthonyecruz@gmail.com>
2020-10-30 19:01:39 -0400

    questions and answers appear

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/trivia';
+import NavBar from './components/nav_bar';
+import Trivia from './components/trivia';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar></NavBar>
+        <Trivia></Trivia>
       </header>
     </div>
   );
--- src/components/trivia.jsx
+++ src/components/trivia.jsx
@@ -1,16 +1,68 @@
 import { connect } from "react-redux";
 import React from "react";
-import { Navbar, Nav, Form, Button, NavDropdown} from "react-bootstrap"
+import data from "../data/data.json"
+import { ButtonGroup, Button, Form, NavDropdown} from "react-bootstrap"
 
 class Pins extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      questions: [],
+      score: 0
+    }
+  }
+
+  componentDidMount() {
+    this.loadQuestions()
+  }
+
+  question() {
+    if (this.state.questions.length) {
+
+      const question = this.state.questions[0].question
+      const answers = this.state.questions[0].incorrect
+      answers.push(this.state.questions[0].correct)
+      console.log(question)
+      
+      const listedQuestions = answers.map(ele => {
+        return(<Button>{ele}</Button>)
+      })
+      return(
+        <div>
+          <p>
+            {question}
+          </p>
+          <ButtonGroup vertical>
+            {listedQuestions}
+          </ButtonGroup>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
+  loadQuestions() {
+    const questions = this.shuffle(data)
+    questions.slice(0, 10)
+    this.setState({ questions: questions.slice(0, 10)})
+    console.log(this.state)
+  }
+  
+  shuffle(array) {
+    return(array.sort(() => Math.random() - 0.5))
   }
 
   render() {
+    // const questions = JSON.parse(data)
+    // console.log(questions[0])
+    const questions = this.shuffle(data)
+    console.log(this.state)
     return(
       <div>
-        
+        <div>
+          {this.question()}
+        </div>
       </div>
     )
   }
